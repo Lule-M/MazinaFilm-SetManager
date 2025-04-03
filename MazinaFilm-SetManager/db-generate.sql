49093531ad4c70171109b7e25a26b4e29c17264b
Luciano-M <102188476+Luciano-M@users.noreply.github.com>
2025-04-04 00:29:12 +0200

    Add project files.

---
+++ MazinaFilm-SetManager/db-generate.sql
@@ -0,0 +1,39 @@
+﻿CREATE DATABASE mazinafilm;
+GO
+USE mazinafilm;
+GO
+
+-- KREIRANJE TABELA
+
+--    ┌──────────────────┐                                                                 
+--    │      Scena       │░                ┌──────────────────────┐                        
+--    │                  │░0:n         1:1 │       Lokacija       │░                       
+--    │IDScena    :int   ├─────────────────┤                      │░                       
+--    │Redni_broj :int   │░                │IDLokacija     :int   │░                       
+--    │IDLokacija :int   │░                │Naziv_lokacije :string│░                       
+--    │Doba_dana  :string│░                └──────────────────────┘░                       
+--    └─────────┬────────┘░                 ░░░░░░░░░░░░░░░░░░░░░░░░                       
+--     ░░░░░░░░░│░1:n░░░░░░                                                                
+--              │                                                                          
+--              │ 1:n                                                                      
+--     ┌────────┴───────┐                                                                  
+--     │ ScenaZaposleni │░                                                                 
+--     │                │░                                                                 
+--     │IDScena     :int│░                                                                 
+--     │IDZaposleni :int│░                                                                 
+--     └────────┬───────┘░                                                                 
+--      ░░░░░░░░│░1:n░░░░░                                                                 
+--              │                                                                          
+--              │ 0:n                                                                      
+--    ┌─────────┴──────────┐                                                               
+--    │     Zaposleni      │░          ┌────────────────┐           ┌────────────────────┐ 
+--    │                    │░          │ZaposleniOprema │░          │       Oprema       │░
+--    │IDZaposleni :int    │░1:n   0:n │                │░0:n   1:n │                    │░
+--    │Ime         :string ├───────────┤IDZaposleni :int├───────────┤IDOprema :int       │░
+--    │Prezime     :string │░          │IDOprema    :int│░          │Naziv_opreme :string│░
+--    │Radno_mesto :string │░          └────────────────┘░          └────────────────────┘░
+--    └────────────────────┘░           ░░░░░░░░░░░░░░░░░░           ░░░░░░░░░░░░░░░░░░░░░░
+--     ░░░░░░░░░░░░░░░░░░░░░░                                                              
+
+
+CREATE TABLE
